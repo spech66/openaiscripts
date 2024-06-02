001643ba1be23458746ea6df270289864bb42d40
Sebastian Pech <spech@spech.de>
2024-06-02 08:32:20 +0200

    basic json

--- group-brainstorming.py
+++ group-brainstorming.py
@@ -1,10 +1,10 @@
 import os
 import sys
 from datetime import datetime
-import urllib.request
 from dotenv import load_dotenv
 import openai
 from openai import OpenAI
+import json
 
 load_dotenv()
 
@@ -19,15 +19,34 @@ client = OpenAI()
 
 response = ""
 
-print("WIP")
+personas = []
+with open("group_brainstorming_personas.json", "r") as file:
+    personas = json.load(file)
 
-# Store the resposne in a file
-if not os.path.isdir("group_brainstorming"):
-    os.mkdir("group_brainstorming")
+print("Participants:")
+for persona in personas:
+    print(f"    {persona['name']}")
 
-cur_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+# try:
+#     completion = client.chat.completions.create(
+#         model="gpt-4o",
+#         messages=[
+#             {"role": "user", "content": text_prompt}
+#         ]
+#     )
+#     response = completion.choices[0].message.content
+# except openai.OpenAIError as e:
+#   print(e.http_status)
+#   print(e.error)
+#   exit(1)    
 
-with open(f"group_brainstorming/{cur_time}.txt", "w", encoding="utf-8") as f:
-    f.write(text_prompt)
-    f.write("\n\n---\n\n")
-    f.write(response)
+# # Store the response in a file
+# if not os.path.isdir("group_brainstorming"):
+#     os.mkdir("group_brainstorming")
+
+# cur_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+# with open(f"group_brainstorming/{cur_time}.txt", "w", encoding="utf-8") as f:
+#     f.write(text_prompt)
+#     f.write("\n\n---\n\n")
+#     f.write(response)
