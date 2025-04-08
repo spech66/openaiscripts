6595e58cc39defda85acdf34c786fe5b64d104cb
Sebastian Pech <spech@spech.de>
2025-04-08 16:37:14 +0200

    try more fixes on todo

--- ms_todo.py
+++ ms_todo.py
@@ -22,24 +22,34 @@ user_principal_name = os.getenv("MS_ENTRA_ID_USER_PRINCIPAL_NAME")
 # Application Permissions - User.Read.All -> No me endpoints and user id needed
 scopes = ['https://graph.microsoft.com/.default']
 credential = ClientSecretCredential(tenant_id=tenant_id, client_id=client_id, client_secret=client_secret)
-graph_client = GraphServiceClient(credential, scopes)
 
 # Delegate Permissions - User.Read -> me endpoint without user id
 # credential = DeviceCodeCredential(client_id, tenant_id = tenant_id)
 # graph_client = GraphServiceClient(credential, ["User.Read", "Tasks.Read"])
 
 # async def get_current_user():
+#     graph_client = GraphServiceClient(credential, scopes)
 #     user = await graph_client.me.get()
 #    print(user)
 
 async def get_current_user():
     try:
+        graph_client = GraphServiceClient(credential, scopes)
         user = await graph_client.users.by_user_id(user_principal_name).get()
         if user:
             print(f'User: {user.display_name}')
             return user.id
     except APIError as e:
         print(f'Error: {e.error.message}')
+
+async def get_all_users():
+    try:
+        graph_client = GraphServiceClient(credential, scopes)
+        users = await graph_client.users.get()
+        for user in users.value:
+            print(user.display_name)
+    except APIError as e:
+        print(f'Error: {e.error.message}')            
 
 # https://learn.microsoft.com/en-us/graph/tutorials/python-app-only?tabs=aad&tutorial-step=3
 async def get_app_only_token():
@@ -49,6 +59,7 @@ async def get_app_only_token():
 
 async def get_todo_list():
     try:
+        graph_client = GraphServiceClient(credential, scopes)
         todo_lists = await graph_client.users.by_user_id(user_principal_name).todo.lists.get()
         # ERROR => https://github.com/microsoftgraph/msgraph-sdk-python/issues/910
         # :-(
@@ -67,13 +78,15 @@ async def get_todo_list_api(token, userid):
     url = f'https://graph.microsoft.com/v1.0/users/{userid}/todo/lists'
     headers = {
         'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json'
+        # 'Content-Type': 'application/json'
     }
     response = requests.get(url, headers=headers)
     print(response.json())
 
-userid = asyncio.run(get_current_user())
+user_id = asyncio.run(get_current_user())
+# asyncio.run(get_all_users()) # test
 token = asyncio.run(get_app_only_token())
-#asyncio.run(get_todo_list())
-asyncio.run(get_todo_list_api(token, userid))
+# print(f'Token: {token}')
+# asyncio.run(get_todo_list())
+asyncio.run(get_todo_list_api(token, user_id))
 
