b793581b07d85a53a0f2377b2f69f6f5c37f6b75
Sebastian Pech <spech@spech.de>
2026-05-17 09:40:57 +0200

    gpt-4o-transcribe

--- whisper.py
+++ whisper.py
@@ -26,7 +26,7 @@ if audio_file_name.endswith(".ogg"):
 audio_file = open(audio_file_name, "rb")
 client = OpenAI()
 transcript = client.audio.transcriptions.create(
-    model="whisper-1",
+    model="gpt-4o-transcribe",
     file=audio_file,
     language="de",
 )
